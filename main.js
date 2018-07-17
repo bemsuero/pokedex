d608c338e79953551c736976de9ac85efa0ba7b9
Bemilton Suero <bemsuero@yahoo.com>
2018-07-17 16:49:12 -0400

    things are where they should be and look good. time to break it.

--- main.js
+++ main.js
@@ -41,7 +41,27 @@ function start() {
   z.classList.remove("infinite");
   z.classList.remove("flash");
 
-  var y = document.getElementById("pokedex")
+  var y = document.getElementById("infoScreen")
   y.classList.add("revealed");
   y.classList.remove("hidden");
 }
+
+//state and status *** status of 200 means it was successful 503 means internal service error 404 means not found.
+
+//http verbs specify what kind of request you're making (get post patch and delete are main verbs.)
+
+//get - get something from bulletin board. post - put stuff onto a bulletin board. patch - update info. delete - delete it.
+
+//
+
+// function loadDoc() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       document.getElementById("info").innerHTML =
+//       this.responseText;
+//     }
+//   };
+//   xhttp.open("GET", "https://raw.githubusercontent.com/bemsuero/pokerepo/master/test.txt", true);
+//   xhttp.send();
+// }
