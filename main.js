ce0e75ca15b0f5a96fc5df87f4bb894fa0762862
Bemilton Suero <bemsuero@yahoo.com>
2018-07-18 16:47:10 -0400

    DONE DONE DONE DONE DONE but still need to add classes

--- main.js
+++ main.js
@@ -15,7 +15,40 @@ b.src = pokemonPage.startScreen;
 var startFlash = document.getElementById("pressStart");
 startFlash.src = pokemonPage.pressStart;
 
-
+// pokemon = [];
+// totalPokemon = 0;
+//
+//   }
+//   createPokemon() {
+//     var pokemon = {
+//       class Pokemon {
+//         constructor() {
+//           this.pokeName = name;
+//           this.pokeImage = image;
+//           this.pokeNumber = number;
+//           this.pokeType = type;
+//           this.atk = atk;
+//           this.def = def;
+//           this.hp = hp;
+//     }
+//     totalPokemon++;
+//     pokemon.push(pokemon);
+//   }
+// }
+//     owner = {
+//       name: "Trainer";
+//       ownedPokemon: pokemon,
+//       roster function() {
+//         // return this.pokemon;
+//         console.log(users[0]);
+//         console.log(users[1]);
+//         console.log(users[2]);
+//
+//       }
+//     }
+//
+//     var x = owner.roster();
+    //you can call it by just x.
 
 // function flash() {
 //   window.setInterval(flash, 1000);
@@ -72,5 +105,41 @@ document.getElementById("def").innerHTML = myObj.stats[3].base_stat;
     }
   };
   xhttp.open("GET", "https://raw.githubusercontent.com/bemsuero/pokerepo/master/squirtle.txt", true);
+  xhttp.send();
+}
+
+function psyduck() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
+      document.getElementById("pokeName").innerHTML = myObj.name;
+document.getElementById("pokeImage").src = myObj.sprites.front_default;
+document.getElementById("pokeNumber").innerHTML = "Num " + myObj.id;
+document.getElementById("pokeType").innerHTML = myObj.types[0].type.name;
+document.getElementById("atk").innerHTML = myObj.stats[4].base_stat;
+document.getElementById("def").innerHTML = myObj.stats[3].base_stat;
+  document.getElementById("abilityBox").innerHTML = "HP is: " + myObj.stats[5].base_stat +  ". " + "Abilities are " + myObj.abilities[0].ability.name + " and " + myObj.abilities[1].ability.name + ". ";
+    }
+  };
+  xhttp.open("GET", "https://raw.githubusercontent.com/bemsuero/pokerepo/master/psyduck.txt", true);
+  xhttp.send();
+}
+
+function growlithe() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
+      document.getElementById("pokeName").innerHTML = myObj.name;
+document.getElementById("pokeImage").src = myObj.sprites.front_default;
+document.getElementById("pokeNumber").innerHTML = "Num " + myObj.id;
+document.getElementById("pokeType").innerHTML = myObj.types[0].type.name;
+document.getElementById("atk").innerHTML = myObj.stats[4].base_stat;
+document.getElementById("def").innerHTML = myObj.stats[3].base_stat;
+  document.getElementById("abilityBox").innerHTML = "HP is: " + myObj.stats[5].base_stat +  ". " + "Abilities are " + myObj.abilities[0].ability.name + " and " + myObj.abilities[1].ability.name + ". ";
+    }
+  };
+  xhttp.open("GET", "https://raw.githubusercontent.com/bemsuero/pokerepo/master/growlithe.txt", true);
   xhttp.send();
 }
